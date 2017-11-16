17e5b4f2d3394e77a4f23b4634ba2120bd7fd174
suraj kumar <suraj.kumar@girnarsoft.com>
2017-11-17 01:12:35 +0530

    removed demo dir || dublicate entry

--- demo/application/config/authorize_net.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-// Authorize.net Account Info
-$config['api_login_id'] = '9GtU5p2k';
-$config['api_transaction_key'] = '33N7u8KH2B3bd2ed';
-$config['api_url'] = 'https://test.authorize.net/gateway/transact.dll'; // TEST URL
-//$config['api_url'] = 'https://secure.authorize.net/gateway/transact.dll'; // PRODUCTION URL
-
-/* EOF */
--- demo/application/config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//$config['appId'] 			= '145511452300973';
-	//$config['secret'] 			= '4a938687eda4e49beb5e1b2b2e613228';
-	
-	$config['appId'] 			= '253157554868460';
-	$config['secret'] 			= '2ba8d221c19c9205c9125e7d22d5d582';
-?>
